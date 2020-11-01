d4fde565b34c0f41402453c96dd8814cd90631cf
someone <some@a>
2020-11-01 09:55:02 -0800

    tema 1 PN

---
+++ tema-1/mas.ssatr.mihaly/Main.java
@@ -0,0 +1,17 @@
+import org.json.simple.JSONObject;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Place> placesList;
+        ArrayList<Transition> transitionsList;
+        Loader loader=new Loader();
+     JSONObject jsonObject=loader.readJson("pn.json");
+       placesList=loader.readPlaces(jsonObject);
+       transitionsList=loader.readTransitions(jsonObject);
+        Simulation s= new Simulation();
+       s.run(placesList,transitionsList);
+        System.out.println("DONE-------CHECK OUTPUTS");
+    }
+}
